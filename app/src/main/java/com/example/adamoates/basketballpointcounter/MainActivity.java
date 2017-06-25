ce29958e11d49be2d826200ae1a7a0e3e8e6d61d
adamCoderTrader <writingyourcareer@gmail.com>
2017-06-25 04:11:43 -0500

    designed content

--- app/src/main/java/com/example/adamoates/basketballpointcounter/MainActivity.java
+++ app/src/main/java/com/example/adamoates/basketballpointcounter/MainActivity.java
@@ -6,8 +6,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import static com.example.adamoates.basketballpointcounter.R.string.homeTeamScore;
-
 public class MainActivity extends AppCompatActivity {
 
     int twoPoints = 2;
